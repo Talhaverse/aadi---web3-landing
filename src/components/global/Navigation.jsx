e60d8ce8de9067575330357d6e02378948d1f5f7
Syed Talha Ahmed <41064991+Talhaverse@users.noreply.github.com>
2024-04-22 00:54:57 +0500

    ..

--- src/components/global/Navigation.jsx
+++ src/components/global/Navigation.jsx
@@ -5,66 +5,12 @@ import { useState,useEffect } from "react";
 
 function Navigation() {
 
-  const [walletAddress, setWalletAddress] = useState(null);
+  //helper funcs
 
-
-  useEffect(() => {
-      const address = sessionStorage.getItem("accountAddress");  
-      if (address) {
-        setWalletAddress(address);
-      }
- 
-  }, []);
-
-
-   const connectMetaMaskFunc = async (SelectedChain) => {
-      console.log("SelectedChain", SelectedChain);
-      if (window.ethereum) {
-        try {
-          // Switching chain and connecting to MetaMask
-          await window.ethereum.request({
-            method: 'wallet_switchEthereumChain',
-            params: [{ chainId: SelectedChain === "Polygon" ? "0x13881" : "0x5" }],
-          });
-          const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-    
-          // Handling the connected account
-          handleConnectedAccount(accounts);
-    
-          // Adding an event listener to handle account changes
-          window.ethereum.on('accountsChanged', handleConnectedAccount);
-    
-        } catch (error) {
-          console.error('Error:', error);
-        }
-      } else {
-        console.error('MetaMask not found.');
-      }
-      // const SUPPORTED_CHAINS = {
-      //   Polygon: "0x89",
-      //   "Ethereum mainnet": "0x1",
-      // };
-
-
-    }
-
-    const handleConnectedAccount = (accounts) => {
-      if (accounts.length > 0) {
-        const currentAccount = accounts[0];
-        console.log('Connected Account Address:', currentAccount);
-    
-        // Store the connected account address in session storage
-        sessionStorage.setItem("accountAddress", currentAccount);
-        sessionStorage.setItem("walletType", "metaMask");
-    
-        // Update the walletAddress state
-        setWalletAddress(currentAccount);
-    
-        // You can perform further actions with the connected account here.
-      } else {
-        console.log('No connected accounts.');
-      }
-    };
+   function requestAccount(){
+    console.log("requesting account ...")
+  }
+  
 
   return (
     
@@ -80,16 +26,14 @@ function Navigation() {
 
   
 
-<a
-          onClick={() => connectMetaMaskFunc("Ethereum mainnet")}
+<button
+          onClick={requestAccount}
           className=" connect-button w-full bg-carrot duration-200 font-display hover:bg-black text-white lg:w-auto px-6 py-4 sm:rounded-full text-center text-xs uppercase"
         >
-          {walletAddress
-            ? `${walletAddress.substring(0, 6)}...${walletAddress.substring(
-                walletAddress.length - 4
-              )}`
-            : "Connect Wallet"}
-            </a>
+          Connect Wallet
+            </button>
+
+            <h3>0x11...</h3>
              
             </div>
           </div>
